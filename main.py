f0d0a83a473f316553e8fe90cde17c5229896c12
Nikita <147915166+EfiNik92@users.noreply.github.com>
2026-09-23 13:50:53 +0300

    test2309

--- main.py
+++ main.py
@@ -6,7 +6,8 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    prinpip install websockets
+t(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print('Hello 777')
 
 
